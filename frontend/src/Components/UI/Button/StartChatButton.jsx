f0ef008e6d2ba53e5c54b4683367a9d35ef755b8
VarlaamGraime <108095273+VarlaamGraime@users.noreply.github.com>
2022-09-17 11:48:53 +0300

    fix

--- frontend/src/Components/UI/Button/StartChatButton.jsx
+++ frontend/src/Components/UI/Button/StartChatButton.jsx
@@ -1,32 +1,44 @@
 import React from 'react';
+import { useState } from 'react';
+import './StartChatButton.module.css'
+import 'animate.css';
+
 
 
 
 
 const StartChatButton = ({ nickname }) => {
+    const [isPostsLoading, setisPostsLoading] = useState(false)
+
 
 
     function submitHandler() {
+        setisPostsLoading(true);
 
-        fetch('http://Localhost:8000/chat/', {
+        setTimeout(async () => {
 
-            method: "POST",
-            headers: {
-                Accept: 'application/json',
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({ nickname }),
-
-
-        })
-
-
+            fetch('http://Localhost:8000/chat/', {
+                method: "POST",
+                headers: {
+                    Accept: 'application/json',
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({ nickname }),
+            })
+            setisPostsLoading(false);
+        }, 3000)
 
     }
     return (
 
         <div>
-            <button type="button" className="btn btn-info margin_element_name" onClick={submitHandler}>Ok</button>
+
+            {isPostsLoading
+                ? <button type="button" className="btn custom btn-block btn-info margin_element_name animate__animated animate__pulse animate__infinite" onClick={submitHandler}>Loading...</button>
+                : <button type="button" className="btn custom btn-block btn-info margin_element_name " onClick={submitHandler}>  Okay  </button>
+
+            }
+
         </div>
     );
 };
